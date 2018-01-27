5b8b3b24c3c5496781cfe6fc7babc37fd5fa08c5
stonedcoder <vraunaq1213@ghmail.com>
2018-01-27 12:57:25 +0530

    AI services added
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- app/src/main/java/com/coding/stonedcoder/dialogflowandroid/MainActivity.kt
+++ app/src/main/java/com/coding/stonedcoder/dialogflowandroid/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.coding.stonedcoder.dialogflowandroid
 
-import ai.api.AIConfiguration.SupportedLanguages
-import ai.api.android.AIConfiguration
-import ai.api.android.AIService
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
-import com.dialogflow.dev.dialogflow.app.DFinstance
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var aiService: AIService
+    private lateinit var listenButton: Button
+    private lateinit var txtView: TextView
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeWidgets()
+        aiService = createAiService()
     }
 }
